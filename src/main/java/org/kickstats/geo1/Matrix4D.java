9f11abfd57b53524b0c83b308ce4193fb8fa6c55
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-01 16:37:56 -0500

    feat: Created a constructor stub for the Matrix4D class.
    Constructor is designed to create a 4x4 identity matrix.

--- src/main/java/org/kickstats/geo1/Matrix4D.java
+++ src/main/java/org/kickstats/geo1/Matrix4D.java
@@ -10,4 +10,11 @@ public class Matrix4D {
     
     double[][] elements = new double[4][4];
     
+    /**
+     * Creates a 4x4 identity matrix.
+     */
+    public Matrix4D() {
+        
+    }// Matrix4D
+    
 }// Matrix4D

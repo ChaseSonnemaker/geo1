6de3e927b9c04f34fdd82cf76fb5f7d5ebcc6668
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-01 16:10:08 -0500

    feat: Created a constructor method for Vector4D.

--- src/main/java/org/kickstats/geo1/Vector4D.java
+++ src/main/java/org/kickstats/geo1/Vector4D.java
@@ -13,4 +13,10 @@ public class Vector4D {
     double p2;
     double p3;
     
+    public Vector4D(double p0, double p1, double p2, double p3) {
+        this.p0 = p0;
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }// Vector4D(double, double, double, double)
 }// Vector4D

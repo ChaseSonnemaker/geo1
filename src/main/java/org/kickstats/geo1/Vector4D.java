75451992e0f6efcc17eee4ae77cb1ce4668ff899
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-01 16:25:45 -0500

    feat: Created get methods for all instance variables in the Vector4D class.

--- src/main/java/org/kickstats/geo1/Vector4D.java
+++ src/main/java/org/kickstats/geo1/Vector4D.java
@@ -19,4 +19,44 @@ public class Vector4D {
         this.p2 = p2;
         this.p3 = p3;
     }// Vector4D(double, double, double, double)
+
+    
+    /**
+     * Returns the value at position 0 in this vector.
+     * 
+     * @return The value of the first element in this vector.
+     */
+    public double getP0() {
+        return p0;
+    }// getP0()
+
+    
+    /**
+     * Returns the value at position 1 in this vector.
+     * 
+     * @return The value of the first element in this vector.
+     */
+    public double getP1() {
+        return p1;
+    }// getP1()
+
+    
+    /**
+     * Returns the value at position 2 in this vector.
+     * 
+     * @return The value of the first element in this vector.
+     */
+    public double getP2() {
+        return p2;
+    }// getP2()
+
+    
+    /**
+     * Returns the value at position 3 in this vector.
+     * 
+     * @return The value of the first element in this vector.
+     */
+    public double getP3() {
+        return p3;
+    }// getP3()
 }// Vector4D

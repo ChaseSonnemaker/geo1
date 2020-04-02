fa1e1ac12f92217dc341e40b1539709daf739f85
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-01 20:00:34 -0500

    test: Finished the test for getP2 in the Vector2D class.

--- src/test/java/org/kickstats/geo1/Vector4DTest.java
+++ src/test/java/org/kickstats/geo1/Vector4DTest.java
@@ -44,12 +44,10 @@ public class Vector4DTest {
     @Test
     public void testGetP2() {
         System.out.println("getP2");
-        Vector4D instance = null;
-        double expResult = 0.0;
+        Vector4D instance = new Vector4D(4, 3, 2, 1);
+        double expResult = 2.0;
         double result = instance.getP2();
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expResult, result, 1E-8);
     }
 
     /**

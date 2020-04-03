56faf3c4448c90ad694e195ab0dcdd4fa697e438
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-03 14:49:57 -0500

    feat: Created instance variables and constants in LoadingScreen.

--- src/main/java/org/kickstats/geo1/LoadingScreen.java
+++ src/main/java/org/kickstats/geo1/LoadingScreen.java
@@ -1,7 +1,10 @@
 
 package org.kickstats.geo1;
 
+import java.awt.Color;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JFrame;
 
 /**
@@ -11,4 +14,13 @@ import javax.swing.JFrame;
  */
 public class LoadingScreen extends JFrame implements ActionListener {
     
+    private final int FRAME_WIDTH = 800;
+    private final int FRAME_HEIGHT = 800;
+    private final String FRAME_TITLE = "Loading...";
+    private final String CONTAINED = "Contained";
+    private final String BREAKOUT = "Breakout";
+    private final String DRAGONS = "Here there be dragons";
+    private final String SCP = "SCP";
+            
+    private final List<String> themes = new ArrayList<>();
 }// LoadingScreen()

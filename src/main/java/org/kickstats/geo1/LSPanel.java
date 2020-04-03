f6ab5529b588caf2f45ef6fc504bfc790762e887
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-03 14:38:29 -0500

    feat: Created classes used to create a loading screen animation.

---
+++ src/main/java/org/kickstats/geo1/LSPanel.java
@@ -0,0 +1,14 @@
+
+package org.kickstats.geo1;
+
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+/**
+ * Support class for LoadingScreen.java.
+ * 
+ * @author Chase Sonnemaker with code from Leon Tabak.
+ */
+public class LSPanel extends JFrame implements ActionListener {
+    
+}// LSPanel()
---
+++ src/main/java/org/kickstats/geo1/LoadingScreen.java
@@ -0,0 +1,14 @@
+
+package org.kickstats.geo1;
+
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+/**
+ * Creates a loading screen with the SCP symbol on it.
+ * 
+ * @author Chase Sonnemaker using code from Leon Tabak.
+ */
+public class LoadingScreen extends JFrame implements ActionListener {
+    
+}// LoadingScreen()

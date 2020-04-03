00e81a7c904275584107dfcaf4eb880d481fd825
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-02 23:22:25 -0500

    feat: Created a spinning vibrating cube.

--- src/main/java/org/kickstats/geo1/SwingPanel.java
+++ src/main/java/org/kickstats/geo1/SwingPanel.java
@@ -1,5 +1,6 @@
 package org.kickstats.geo1;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -14,15 +15,21 @@ import javax.swing.Timer;
 
 public class SwingPanel extends JPanel implements ActionListener {
 
-    private double ctr1x1 = -0.5;
-    private double ctr1y1 = 0.5;
-    private double ctr1x2 = 0.5;
-    private double ctr1y2 = -0.5;
-    private double x1 = 1;
-    private double x2 = -1;
-    private double y1 = 0.0;
-    private double y2 = 0.0;
-    private double factorChange = 0.3;
+    private double ctr1x1 = 0;
+    private double ctr1x2 = 0;
+    private double ctr1x3 = 0;
+    private double ctr1x4 = 0;
+    private double fake = 0;
+    private double ctr1y1 = 0;
+    private double ctr1y2 = 0;
+    private double ctr1y3 = 0;
+    private double ctr1y4 = 0;
+    private double x1 = 0.8;
+    private double x2 = -0.8;
+    private double y1 = 0.8;
+    private double y2 = -0.8;
+    private double factorChange = 0.1;
+    private double angle = 0;
     
     private Color color = Color.red;
 
@@ -52,23 +59,48 @@ public class SwingPanel extends JPanel implements ActionListener {
         scaling.setToScale(w / 2, h / 2);
         AffineTransform translation = new AffineTransform();
         translation.setToTranslation(1.0, 1.0);
+        AffineTransform rotate = new AffineTransform();
+        rotate.setToRotation(angle, 0, 0);
 
         transform.concatenate(scaling);
         transform.concatenate(translation);
+        transform.concatenate(rotate);
 
         // Replace this block of code that creates
         // an ellipse with your own code that draws
         // something else
         // Make sure that all geometry fits in a square
         // whose corners are (-1, -1) and (+1, +1)
+        this.angle = this.angle + 0.1;
         ctr1y1 = ctr1y1 + factorChange;
-        ctr1y2 = ctr1y2 - factorChange;
-        CubicCurve2D.Double curve = new CubicCurve2D.Double(x1, y1, ctr1x1,
-        ctr1y1, ctr1x2, ctr1y2, x2, y2);
+        ctr1y3 = ctr1y3 - factorChange;
+        ctr1x2 = ctr1x2 + factorChange;
+        ctr1x4 = ctr1x4 - factorChange;
+        QuadCurve2D.Double curve1 = new QuadCurve2D.Double(x1, y2, ctr1x1,
+        ctr1y1, x2, y2);
+        
+        QuadCurve2D.Double curve2 = new QuadCurve2D.Double(x2, y2, ctr1x2,
+        ctr1y2, x2, y1);
+        
+        QuadCurve2D.Double curve3 = new QuadCurve2D.Double(x2, y1, ctr1x3,
+        ctr1y3, x1, y1);
+        
+        QuadCurve2D.Double curve4 = new QuadCurve2D.Double(x1, y1, ctr1x4,
+        ctr1y4, x1, y2);
 
-        Shape shape = transform.createTransformedShape(curve);
+        Shape shape1 = transform.createTransformedShape(curve1);
+        Shape shape2 = transform.createTransformedShape(curve2);
+        Shape shape3 = transform.createTransformedShape(curve3);
+        Shape shape4 = transform.createTransformedShape(curve4);
         g2D.setColor(this.getColor());
-        g2D.draw(shape);
+        
+        BasicStroke stroke = new BasicStroke(10f);
+        
+        g2D.setStroke(stroke);
+        g2D.draw(shape1);
+        g2D.draw(shape2);
+        g2D.draw(shape3);
+        g2D.draw(shape4);
     } // paintComponent( Graphics )
 
     @Override
@@ -80,10 +112,11 @@ public class SwingPanel extends JPanel implements ActionListener {
         // Rotate? (There's an AffineTransform for that, too.)
         // Change color?
 
-        if (this.ctr1y1 > 2) {
+        this.fake = this.fake + this.factorChange;
+        if (this.fake < -1.6 ) {
             this.factorChange = -this.factorChange;
         } // if
-        else if (this.ctr1y1 < -2) {
+        else if (this.fake > 0) {
             this.factorChange = -this.factorChange;
         } // else if
 

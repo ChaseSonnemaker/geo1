557b695cf8ecc39c82c383f3bbc4db1603a976dc
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-04 17:46:40 -0500

    feat: Made symbol and loading sign spin.

--- src/main/java/org/kickstats/geo1/Symbol.java
+++ src/main/java/org/kickstats/geo1/Symbol.java
@@ -16,6 +16,7 @@ import java.awt.geom.Line2D;
 import java.awt.geom.Path2D;
 import java.awt.geom.QuadCurve2D;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 /**
  * Contains methods used to build the loading symbols.
@@ -64,9 +65,21 @@ public class Symbol extends JPanel implements ActionListener {
     double loadCenterX = 0;
     double loadCenterY = 0.525;
     int loadLines = 8;
+    
+    double rotateSymbol = 360;
+    double rotateLoad = 60;
+    double moveSymbol = (2 * Math.PI) / this.rotateSymbol;
+    double moveLoad = (2 * Math.PI) / this.rotateLoad;
+    double angleSymbol = 0;
+    double angleLoad = 0;
         
     private Color color = Color.white;
-     
+    
+    public Symbol() {
+       Timer timer = new Timer(50, this);
+       timer.start();
+    } // SwingPanel()
+    
     public Color getColor() {
         return this.color;
     }// getColor()
@@ -95,6 +108,21 @@ public class Symbol extends JPanel implements ActionListener {
         BasicStroke stroke = new BasicStroke(15f);
         g2D.setStroke(stroke);
         
+        AffineTransform transform2 = new AffineTransform();
+        AffineTransform rotate2 = new AffineTransform();
+        rotate2.setToRotation(ARROW_ANGLE, centerX, centerY);
+        transform2.concatenate(scaling);
+        transform2.concatenate(translation);
+        transform2.concatenate(rotate2);
+        
+        AffineTransform transform3 = new AffineTransform();
+        AffineTransform rotate3 = new AffineTransform();
+        rotate3.setToRotation(-ARROW_ANGLE, centerX, centerY);
+        transform3.concatenate(scaling);
+        transform3.concatenate(translation);
+        transform3.concatenate(rotate3);
+
+        
         //First Circle
         double d = 2 * this.radius;
         double ulx = this.centerX - this.radius;
@@ -105,57 +133,6 @@ public class Symbol extends JPanel implements ActionListener {
         
         BasicStroke stroke2 = new BasicStroke(8f);
         g2D.setStroke(stroke2);
-        
-        //Arrow 1
-        Line2D.Double line1 = new Line2D.Double(origX, origY, origX2, origY2);
-        Shape arrow1 = transform.createTransformedShape(line1);
-        Path2D.Double arrowhead1 = new Path2D.Double();
-        arrowhead1.moveTo(arpX, arpY);
-        arrowhead1.lineTo(arX, arY);
-        arrowhead1.lineTo(arX2, arY2);
-        Shape arrowFin1 = transform.createTransformedShape(arrowhead1);
-        g2D.draw(arrow1);
-        g2D.fill(arrowFin1);
-        
-        //Arrow2
-        AffineTransform transform2 = new AffineTransform();
-        AffineTransform rotate2 = new AffineTransform();
-        rotate2.setToRotation(ARROW_ANGLE, centerX, centerY);
-        
-        transform2.concatenate(scaling);
-        transform2.concatenate(translation);
-        transform2.concatenate(rotate2);
-
-        
-        Line2D.Double line2 = new Line2D.Double(origX, origY, origX2, origY2);
-        Shape arrow2 = transform2.createTransformedShape(line2);
-        Path2D.Double arrowhead2 = new Path2D.Double();
-        arrowhead2.moveTo(arpX, arpY);
-        arrowhead2.lineTo(arX, arY);
-        arrowhead2.lineTo(arX2, arY2);
-        Shape arrowFin2 = transform2.createTransformedShape(arrowhead2);
-        g2D.draw(arrow2);
-        g2D.fill(arrowFin2);
-        
-        
-        //Arrow3
-        AffineTransform transform3 = new AffineTransform();
-        AffineTransform rotate3 = new AffineTransform();
-        rotate3.setToRotation(-ARROW_ANGLE, centerX, centerY);
-        
-        transform3.concatenate(scaling);
-        transform3.concatenate(translation);
-        transform3.concatenate(rotate3);
-        
-        Line2D.Double line3 = new Line2D.Double(origX, origY, origX2, origY2);
-        Shape arrow3 = transform3.createTransformedShape(line3);
-        Path2D.Double arrowhead3 = new Path2D.Double();
-        arrowhead3.moveTo(arpX, arpY);
-        arrowhead3.lineTo(arX, arY);
-        arrowhead3.lineTo(arX2, arY2);
-        Shape arrowFin3 = transform3.createTransformedShape(arrowhead3);
-        g2D.draw(arrow3);
-        g2D.fill(arrowFin3);
         
         BasicStroke stroke3 = new BasicStroke(5f);
         g2D.setStroke(stroke3);
@@ -212,6 +189,47 @@ public class Symbol extends JPanel implements ActionListener {
         Shape arcFin3 = transform3.createTransformedShape(arc3);
         g2D.draw(arcFin3);
         
+        //Symbol Rotate
+        AffineTransform symbol = new AffineTransform();
+        symbol.setToRotation(angleSymbol, centerX, centerY);
+        transform.concatenate(symbol);
+        transform2.concatenate(symbol);
+        transform3.concatenate(symbol);
+        
+        //Arrow 1
+        Line2D.Double line1 = new Line2D.Double(origX, origY, origX2, origY2);
+        Shape arrow1 = transform.createTransformedShape(line1);
+        Path2D.Double arrowhead1 = new Path2D.Double();
+        arrowhead1.moveTo(arpX, arpY);
+        arrowhead1.lineTo(arX, arY);
+        arrowhead1.lineTo(arX2, arY2);
+        Shape arrowFin1 = transform.createTransformedShape(arrowhead1);
+        g2D.draw(arrow1);
+        g2D.fill(arrowFin1);
+        
+        //Arrow2
+        Line2D.Double line2 = new Line2D.Double(origX, origY, origX2, origY2);
+        Shape arrow2 = transform2.createTransformedShape(line2);
+        Path2D.Double arrowhead2 = new Path2D.Double();
+        arrowhead2.moveTo(arpX, arpY);
+        arrowhead2.lineTo(arX, arY);
+        arrowhead2.lineTo(arX2, arY2);
+        Shape arrowFin2 = transform2.createTransformedShape(arrowhead2);
+        g2D.draw(arrow2);
+        g2D.fill(arrowFin2);
+        
+        
+        //Arrow3        
+        Line2D.Double line3 = new Line2D.Double(origX, origY, origX2, origY2);
+        Shape arrow3 = transform3.createTransformedShape(line3);
+        Path2D.Double arrowhead3 = new Path2D.Double();
+        arrowhead3.moveTo(arpX, arpY);
+        arrowhead3.lineTo(arX, arY);
+        arrowhead3.lineTo(arX2, arY2);
+        Shape arrowFin3 = transform3.createTransformedShape(arrowhead3);
+        g2D.draw(arrow3);
+        g2D.fill(arrowFin3);
+        
         //Load Sign
         AffineTransform transform4 = new AffineTransform();
         AffineTransform rotate4 = new AffineTransform();
@@ -223,7 +241,8 @@ public class Symbol extends JPanel implements ActionListener {
             transform4.concatenate(scaling);
             transform4.concatenate(translation);
             rotate4 = new AffineTransform();
-            rotate4.setToRotation(newAngle, loadCenterX, loadCenterY);
+            rotate4.setToRotation(newAngle + angleLoad, 
+                                    loadCenterX, loadCenterY);
             transform4.concatenate(rotate4);
             
             newLine = new Line2D.Double(loadStartX, loadStartY, 
@@ -238,6 +257,15 @@ public class Symbol extends JPanel implements ActionListener {
     
     @Override
     public void actionPerformed(ActionEvent event) {
+        this.angleSymbol = this.angleSymbol + this.moveSymbol;
+        if(this.angleSymbol > (2 * Math.PI)) {
+            this.angleSymbol = this.angleSymbol / this.rotateSymbol;
+        }// if
+        
+        this.angleLoad = this.angleLoad + this.moveLoad;
+        if(this.angleLoad > (2 * Math.PI)) {
+            this.angleLoad = this.angleLoad / this.rotateLoad;
+        }// if
 
         this.repaint();
     } // actionPerformed( ActionEvent )

b8314826451b1e06e34099ddd73feff11008da76
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-05 23:26:33 -0500

    refactor: Removed unused import.

--- src/main/java/org/kickstats/swing/SwingPanel.java
+++ src/main/java/org/kickstats/swing/SwingPanel.java
@@ -8,7 +8,6 @@ import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.CubicCurve2D;
 import java.awt.geom.QuadCurve2D;
 import javax.swing.JPanel;
 import javax.swing.Timer;

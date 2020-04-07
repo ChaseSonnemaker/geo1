c0e70bf65b7d1e5c41d3c6b2252d6f1f92574b20
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-07 16:48:58 -0500

    feat: Created a constructor for Triangle3D.

--- src/main/java/org/kickstats/swing/Triangle3D.java
+++ src/main/java/org/kickstats/swing/Triangle3D.java
@@ -1,6 +1,9 @@
 
 package org.kickstats.swing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * An experiment creating 3D shapes
  * 
@@ -8,5 +11,21 @@ package org.kickstats.swing;
  * @version 7 April 2020
  */
 public class Triangle3D {
+    
+    private final List<Vector> points = new ArrayList<>();
+    
+    public Triangle3D(double length) {
+        for(int i = 0; i < 3; i++) {
+            double percent = (double) i / 3d;
+            double angle = percent * 2 * Math.PI;
+            double radius = length / 2;
+            
+            double x = radius * Math.cos(angle);
+            double y = radius * Math.sin(angle);
+            
+            Vector v = new Vector(x, y, 0.0);
+            this.points.add(v);
+        }// for
+    }// Triangle3D(double)
     
 }// Triangle3D

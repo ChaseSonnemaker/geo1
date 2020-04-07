91ba8142948632e567960e99e2d0687a1f62606f
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-07 15:52:49 -0500

    feat: Created a Triangle3D class.

---
+++ src/main/java/org/kickstats/swing/Triangle3D.java
@@ -0,0 +1,12 @@
+
+package org.kickstats.swing;
+
+/**
+ * An experiment creating 3D shapes
+ * 
+ * @author Chase Sonnemaker with guidance from Leon Tabak's code.
+ * @version 7 April 2020
+ */
+public class Triangle3D {
+    
+}// Triangle3D

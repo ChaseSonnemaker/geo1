e3196d11933ffd48e62b1423c465dc21863cc9e4
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-07 16:07:31 -0500

    feat: Created a SwingPanel3D class for using with Swing3D.

---
+++ src/main/java/org/kickstats/swing/SwingPanel3D.java
@@ -0,0 +1,15 @@
+
+package org.kickstats.swing;
+
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+
+/**
+ * A panel of 3D shapes.
+ * 
+ * @author Chase Sonnemaker with guidance from Leon Tabak's code.
+ * @version 7 April 2020
+ */
+public class SwingPanel3D extends JPanel implements ActionListener {
+    
+}// SwingPanel3D

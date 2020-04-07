253e1893c364322808f848caf897c520e37417f5
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-07 18:48:23 -0500

    feat: Created a triangle in the paintComponent method.

--- src/main/java/org/kickstats/swing/SwingPanel3D.java
+++ src/main/java/org/kickstats/swing/SwingPanel3D.java
@@ -4,8 +4,10 @@ package org.kickstats.swing;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.AffineTransform;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -29,7 +31,27 @@ public class SwingPanel3D extends JPanel implements ActionListener {
         super.paintComponent(g);
         Graphics2D g2D = (Graphics2D) g;
         
-        g2D.setColor(Color.white);
+        g2D.setColor(Color.red);
+        
+        int w = this.getWidth();
+        int h = this.getHeight();
+        
+        Matrix scaling = new Matrix();
+        scaling.scale(w / 2, h / 2, 1);
+        Matrix translation = new Matrix();
+        translation.translate(1, 1, 0);
+        
+        Matrix start = scaling.multiply(translation);
+        
+        Triangle3D triangle = new Triangle3D(0.5);
+        triangle.change(start);
+        Shape newTriangle = triangle.getShape();
+        
+        g2D.fill(newTriangle);
+        
+        g2D.setColor(Color.black);
+        g2D.draw(newTriangle);
+        
     }//paintComponent(Graphics)    
         
     @Override

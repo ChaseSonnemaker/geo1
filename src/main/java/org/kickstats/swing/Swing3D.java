4037ffc62b468e33574ebecb154b3d7f9855649c
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-07 15:58:32 -0500

    feat: Created a Swing3D class for showing off 3D shapes.

---
+++ src/main/java/org/kickstats/swing/Swing3D.java
@@ -0,0 +1,12 @@
+
+package org.kickstats.swing;
+
+/**
+ * An environment for working with 3D shapes.
+ * 
+ * @author Chase Sonnemaker with guidance from Leon Tabak's code.
+ * @version 7 April 2020
+ */
+public class Swing3D {
+    
+}// Swing3D

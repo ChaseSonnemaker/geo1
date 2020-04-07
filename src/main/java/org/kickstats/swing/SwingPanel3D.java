9e11354616ad9deb650c4423da7813128b1da603
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-07 16:20:15 -0500

    feat: Added the basic body of SwingPanel3D.

--- src/main/java/org/kickstats/swing/SwingPanel3D.java
+++ src/main/java/org/kickstats/swing/SwingPanel3D.java
@@ -1,8 +1,13 @@
 
 package org.kickstats.swing;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 /**
  * A panel of 3D shapes.
@@ -11,5 +16,26 @@ import javax.swing.JPanel;
  * @version 7 April 2020
  */
 public class SwingPanel3D extends JPanel implements ActionListener {
+    
+    
+    public SwingPanel3D() {
+       Timer timer = new Timer(50, this);
+       timer.start();
+    } // SwingPanel3D()
+    
+    
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2D = (Graphics2D) g;
+        
+        g2D.setColor(Color.white);
+    }//paintComponent(Graphics)    
+        
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        this.repaint();
+    }// actionPerformed(ActionEvent)
+    
     
 }// SwingPanel3D

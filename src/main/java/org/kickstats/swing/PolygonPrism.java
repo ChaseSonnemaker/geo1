d76a72f64bae1fc0fff07ff115a8e6cb2c84ecdf
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-08 14:46:47 -0500

    feat: Create getFace1 method in PolygonPrism.

--- src/main/java/org/kickstats/swing/PolygonPrism.java
+++ src/main/java/org/kickstats/swing/PolygonPrism.java
@@ -28,36 +28,36 @@ public class PolygonPrism {
         this.shape2Points = points2;
     }// PolygonPrism(int, double)
     
-    public void change(Matrix m) {
-        this.primary1 = this.primary1.changeShape(m);
-        this.primary2 = this.primary2.changeShape(m);
-        List<Polygon3D> newList = new ArrayList<>();
-        for(Polygon3D p : this.rectangles) {
-            newList.add(p.changeShape(m));
-        }// for
-        this.rectangles = newList;
-    }// change(Matrix)
-
+//    public void change(Matrix m) {
+//        this.primary1 = this.primary1.changeShape(m);
+//        this.primary2 = this.primary2.changeShape(m);
+//        List<Polygon3D> newList = new ArrayList<>();
+//        for(Polygon3D p : this.rectangles) {
+//            newList.add(p.changeShape(m));
+//        }// for
+//        this.rectangles = newList;
+//    }// change(Matrix)
+//
     /**
      * @return the primary1
      */
-    public Polygon3D getPrimary1() {
-        return primary1;
-    }
-
-    /**
-     * @return the primary2
-     */
-    public Polygon3D getPrimary2() {
-        return primary2;
-    }
-
-    /**
-     * @return the rectangles
-     */
-    public List<Polygon3D> getRectangles() {
-        return rectangles;
-    }
-    
-    
+    public Polygon3D getFace1() {
+        return new Polygon3D(this.shape1Points);
+    }// getFace1
+//
+//    /**
+//     * @return the primary2
+//     */
+//    public Polygon3D getPrimary2() {
+//        return primary2;
+//    }
+//
+//    /**
+//     * @return the rectangles
+//     */
+//    public List<Polygon3D> getRectangles() {
+//        return rectangles;
+//    }
+//    
+//    
 }// PolygonPrism

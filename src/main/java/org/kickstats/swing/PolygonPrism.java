ba5c768d0648bff129ed0d051d135bf159727e6b
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-07 19:21:04 -0500

    feat: Created a PolygonPrism class.

---
+++ src/main/java/org/kickstats/swing/PolygonPrism.java
@@ -0,0 +1,14 @@
+
+package org.kickstats.swing;
+
+/**
+ * Creates the elements needed to create a 3D polygon prism.
+ * 
+ * @author Chase Sonnemaker 
+ * @version 7 April 2020
+ */
+public class PolygonPrism {
+    
+    
+    
+}// PolygonPrism

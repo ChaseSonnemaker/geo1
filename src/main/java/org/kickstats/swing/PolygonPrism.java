43f6baf7c6be702d00e1b17ff576384dba65ec44
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-07 19:37:11 -0500

    feat: Created a PolygonPrism constructor.

--- src/main/java/org/kickstats/swing/PolygonPrism.java
+++ src/main/java/org/kickstats/swing/PolygonPrism.java
@@ -1,6 +1,9 @@
 
 package org.kickstats.swing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Creates the elements needed to create a 3D polygon prism.
  * 
@@ -9,6 +12,21 @@ package org.kickstats.swing;
  */
 public class PolygonPrism {
     
+    List<Vector> shape1 = new ArrayList<>();
+    List<Vector> shape2 = new ArrayList<>();
     
+    
+    public PolygonPrism(int sides, double radius, double length) {
+        for(int i = 0; i < sides; i++) {
+            double percent = ((double) i) / sides;
+            double angle = percent * 2 * Math.PI;
+            
+            double x = radius * Math.cos(angle);
+            double y = radius * Math.sin(angle);
+            
+            Vector v1 = new Vector(x, y, 0.0);
+            Vector v2 = new Vector(x, y, -length);
+        }// for
+    }// PolygonPrism(int, double)
     
 }// PolygonPrism

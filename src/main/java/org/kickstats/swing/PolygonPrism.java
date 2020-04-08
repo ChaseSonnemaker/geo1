006cd3fba6f35dfdccc46aa93d9d9888644a16bc
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-07 19:49:26 -0500

    feat: Created a change method for PolygonPrism.

--- src/main/java/org/kickstats/swing/PolygonPrism.java
+++ src/main/java/org/kickstats/swing/PolygonPrism.java
@@ -29,4 +29,25 @@ public class PolygonPrism {
         }// for
     }// PolygonPrism(int, double)
     
+    public void change(Matrix m) {
+        int length = this.shape1.size();
+        
+        List<Vector> newShape1 = new ArrayList<>();
+        List<Vector> newShape2 = new ArrayList<>();
+        
+        for(int i = 0; i < length; i++) {
+            Vector v1 = this.shape1.get(i);
+            Vector v2 = this.shape2.get(i);
+            
+            Vector newV1 = m.multiply(v1);
+            Vector newV2 = m.multiply(v2);
+            
+            newShape1.add(newV1);
+            newShape2.add(newV2);
+        }// for
+        
+        this.shape1 = newShape1;
+        this.shape2 = newShape2;
+    }// change(Matrix)
+    
 }// PolygonPrism

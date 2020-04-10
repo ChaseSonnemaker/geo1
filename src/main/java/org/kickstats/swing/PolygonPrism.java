9e7c86b7bd9a0d42c2a1cb79cba4b692650fdf49
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-10 07:24:17 -0500

    docs: Add more detailed Javadoc comments for PolygonPrism class
    This information includes general explanation of the class and method descriptions.

--- src/main/java/org/kickstats/swing/PolygonPrism.java
+++ src/main/java/org/kickstats/swing/PolygonPrism.java
@@ -5,10 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Creates the elements needed to create a 3D polygon prism.
+ * Models a prism in 3D space.
+ * 
+ * Uses a series of vectors which represent vertices and a series of triangular
+ * Polygon3D objects for modeling the prism. Contains methods designed to 
+ * facilitate transformation and visually representation of the prism. 
  * 
  * @author Chase Sonnemaker 
- * @version 7 April 2020
+ * @version 10 April 2020
  */
 public class PolygonPrism {
     
@@ -18,7 +22,18 @@ public class PolygonPrism {
     private Vector shape2Center;
     private List<Polygon3D> triangles = new ArrayList<>();
     
-    
+    /**
+     * Creates an instance of the PolygonPrism class with specified attributes.
+     * 
+     * The prism represented will have main faces that are regular polygons and
+     * will be centered at the origin in 3D space.
+     * 
+     * @param sides The number of sides the each of the main faces should have.
+     * @param radius The radius of the largest circle that completely 
+     * surrounds each of the main faces.
+     * @param height The distance the main faces are from each other (the 
+     * height of the rectangular side faces).
+     */
     public PolygonPrism(int sides, double radius, double height) {
         
         //Finding shape centers
@@ -51,7 +66,16 @@ public class PolygonPrism {
         this.makeTriangles();
     }// PolygonPrism(int, double)
     
-    
+    /**
+     * Creates the triangles that make up this PolygonPrism object.
+     * 
+     * Uses the vertices stored in this PolygonPrism object to construct a list 
+     * of triangular Polygon3D objects out of the main and side faces. This 
+     * list is then stored in this PolygonPrism object. Used to 
+     * construct the initial triangle list and to update the stored triangle 
+     * list based on transformations made to the vertices of this 
+     * PolygonPrism object.
+     */
     public void makeTriangles() {
         List<Polygon3D> triangleList = new ArrayList<>();
         
@@ -126,7 +150,15 @@ public class PolygonPrism {
         this.triangles = triangleList;
     }// makeTriangles()
     
-    
+    /**
+     * Transforms this PolygonPrism object to reflect some 3D transformation 
+     * modeled by a 4x4 matrix.
+     * 
+     * Allows the prism to be moved, scaled, and rotated, updating all of the 
+     * stored vector vertices and Polygon3D objects accordingly.
+     * 
+     * @param m A 4x4 matrix modeling a 3D transformation.
+     */
     public void change(Matrix m) {
         
         for(int i = 0; i < this.shape1Points.size(); i++) {
@@ -146,7 +178,22 @@ public class PolygonPrism {
         this.makeTriangles();
     }// change(Matrix)
 
-    
+    /**
+     * Orders and returns the list of triangular Polygon3D objects stored in 
+     * this object based on the smallest z-axis coordinate in each triangle.
+     * 
+     * Designed to return a list of Polygon3D objects that can then be 
+     * converted to shapes and visually represented. Uses a selection sort 
+     * algorithm to order the list from from smallest smallest z-axis coordinate 
+     * to largest. This is an attempt to solve the issue of which faces should 
+     * be drawn first when visually representing a 3D prism so that hidden faces 
+     * are not drawn over faces that would be in view. While not a perfect 
+     * solution, this works very well for prisms with large heights and many 
+     * main face sides.
+     * 
+     * @return A list of triangular Polygon3D objects that make up this prism
+     * and is ordered from the smallest smallest z-axis coordinate to largest.
+     */
     public List<Polygon3D> getOrderedShapes() {
         List<Polygon3D> shapeList = this.triangles;
         int length = shapeList.size();

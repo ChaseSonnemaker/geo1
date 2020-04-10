8a925a69a978774258dfe8111759238783555d57
ChaseSonnemaker <CSonnemaker21@cornellcollege.edu>
2020-04-09 20:03:40 -0500

    feat: Remove excess commented out code in paintComponent method
    Located in SwingPanel3D class.

--- src/main/java/org/kickstats/swing/SwingPanel3D.java
+++ src/main/java/org/kickstats/swing/SwingPanel3D.java
@@ -62,8 +62,7 @@ public class SwingPanel3D extends JPanel implements ActionListener {
         super.paintComponent(g);
         Graphics2D g2D = (Graphics2D) g;
         
-        g2D.setColor(Color.black);
-                
+        //Create AffineTransform for initial 
         int w = this.getWidth();
         int h = this.getHeight();
         
@@ -75,10 +74,6 @@ public class SwingPanel3D extends JPanel implements ActionListener {
         transform.concatenate(scale);
         transform.concatenate(translate);
         
-//        Matrix scale = new Matrix();
-//        scale.scale(w/2, h/2, 1);
-//        Matrix translate = new Matrix();
-//        translate.translate(1, 1, 1);
 
         Matrix rotationX = new Matrix();
         Matrix rotationY = new Matrix();
@@ -89,57 +84,10 @@ public class SwingPanel3D extends JPanel implements ActionListener {
         rotationZ.rotateZ(angle);
         
         Matrix rotate = rotationY.multiply(rotationX);
-        
-//        Matrix rotation2 = new Matrix();
-//        rotation2.rotateY(angle);
-        
-//        Matrix originalTransform = scale.multiply(translate);
-//        Matrix transform = originalTransform.multiply(rotation);
-        
-//        Matrix transform2 = originalTransform.multiply(rotation2);
-        
-//        Polygon3D thing = new Polygon3D(6, 0.8, 0);
-//        thing.change(transform);
-//        Shape newThing = thing.getShape();
-//        System.out.println("OG");
-//        for(Vector v : thing.getPoints()) {
-//            System.out.println(v);
-//        }
-//        g2D.draw(newThing);
 
         PolygonPrism prism = new PolygonPrism(54, 0.8, 0.5);
         prism.change(rotate);
-//        Polygon3D face1 = prism.getFace1();
-////        face1.change(transform);
-//        Shape poly1 = face1.getShape();
-//        System.out.println("test");
-//        for(Vector v : face1.getPoints()) {
-//            System.out.println(v);
-//        }
-//        g2D.draw(poly1);
-//        
-//        
-//        Polygon3D face2 = prism.getFace2();
-////        face2.change(transform);
-//        Shape poly2 = face2.getShape();
-//        System.out.println("test2");
-//        for(Vector v : face2.getPoints()) {
-//            System.out.println(v);
-//        }
-//        g2D.draw(poly2);
-//        
-//        
-//        List<Polygon3D> recs = prism.getRectangles();
-//        for(Polygon3D p : recs) {
-//            Shape newP = p.getShape();
-//            g2D.draw(newP);
-//        }
-//        Vector lightSource = new Vector(0.5, 0.5, 0.5);
-//        lightSource = transform2.multiply(lightSource);
-//        
-//        double origR = 102.0 / 255.0;
-//        double origG = 0.0 / 255.0;
-//        double origB = 153.0 / 255.0;
+
         Color color = Color.blue;
         double ambientColor = 0.2;
         Vector lightV = new Vector(1.0, 2.0, 4.0);
@@ -149,10 +97,6 @@ public class SwingPanel3D extends JPanel implements ActionListener {
         for(Polygon3D p : toDraw) {
             Shape s = p.getShape();
             s = transform.createTransformedShape(s);
-            
-//            float newR = (float) p.getNewColorValue(origR, lightSource);
-//            float newG = (float) p.getNewColorValue(origG, lightSource);
-//            float newB = (float) p.getNewColorValue(origB, lightSource);
             
             int red = color.getRed();
             int green = color.getGreen();
